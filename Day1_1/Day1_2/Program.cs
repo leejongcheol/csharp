6cbc1720709ddddbd47e584f86cbadb7c7e0c048
Lee Jong Cheol <mylife68@nate.com>
2018-10-27 12:14:28 +0900

    C# Day1

---
+++ Day1_1/Day1_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Day1_2
+{
+    class MyConstants
+    {
+        public const double PI = 3.14;
+        public const int MYAGE = 22;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius = 2;
+            double area = MyConstants.PI * (radius * radius);
+            Console.WriteLine("Area = {0}, Age={1}", 
+                                                   area, MyConstants.MYAGE);
+            //MyConstants.MYAGE = 20; //오류
+            const string name = "홍길동";
+
+            Console.WriteLine("name : " + name);
+            //name = "김길동"; //오류
+  
+        }
+    }
+}

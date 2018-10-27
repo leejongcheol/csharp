46234dac1aa3a3df595c1d329be22b56ba27a3cf
Lee Jong Cheol <mylife68@nate.com>
2018-10-27 14:40:17 +0900

    www

---
+++ Day1_1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿class Emp {
+    public static string name;
+    public static void GoToOffice() {
+        System.Console.WriteLine(name + "이(가) 출근한다.");
+    }
+}
+class EmpTest {
+    static void Main(string[] args)  {
+        Emp.name = "홍";
+        System.Console.WriteLine(Emp.name);
+        Emp.GoToOffice();
+        int i = 9;
+        System.Console.WriteLine(default(int));
+    }
+}
---
+++ Day1_1/Day1_3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Day1_3{
+    class Program    {
+        static void Main(string[] args)
+        {
+            string s = args[0];
+            int number = int.Parse(s);
+            for(int i=0; i <= number; i+=2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
